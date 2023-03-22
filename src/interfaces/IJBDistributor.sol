802a74b2524b1c1fa25a3f1644b9324e76059aed
viraj124 <virajm72@gmail.com>
2023-03-22 23:33:00 +0530

    chore: update interface

--- src/interfaces/IJBDistributor.sol
+++ src/interfaces/IJBDistributor.sol
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import { JBSplitAllocationData } from "@juicebox/structs/JBSplitAllocationData.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /**
  * @title   JBDistributor
  * @notice 
  * @dev 
  */
 interface IJBDistributor {
-    function deposit(uint256 _depositAmount) external;
 
-// INSERT JUICY CODE HERE
+    function currentClaimable(uint256 _tokenId) external view returns(IERC20[] memory _tokens, uint256[] memory _claimableAmounts);
+    
+    function getBasket() external view returns (IERC20[] memory _token, uint256[] memory _distributableAmount);
+    
+    function claim(uint256 _tokenId) external;
+    
+    function claim(uint256[] calldata _tokenId) external;
+    
+    function allocate(JBSplitAllocationData calldata _data) external payable;
 
-
+    function addAssetToBasket(IERC20 _token) external;
 }
