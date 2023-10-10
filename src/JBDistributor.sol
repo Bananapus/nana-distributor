31099836f9bddf884fe9620730007fd00de5076b
mejango <mejango@protonmail.com>
2023-10-10 18:00:14 -0400

    syntax bug

--- src/JBDistributor.sol
+++ src/JBDistributor.sol
@@ -127,7 +127,7 @@ abstract contract JBDistributor is IJBDistributor {
                 _tokenId = _tokenIds[_j];
 
                 // Keep a reference to the amount of tokens.
-                uint256 _tokenAmount = _distributable * _tokenStake(_tokenId]) / _totalStakeAmount;
+                uint256 _tokenAmount = _distributable * _tokenStake(_tokenId) / _totalStakeAmount;
 
                 // Make sure this token hasn't already been claimed (check might not be needed)
                 if (vestingTokenAmountAtRoundOf[_tokenId][_vestingReleaseRound][_token] != 0) {
